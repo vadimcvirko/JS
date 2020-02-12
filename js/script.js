7ac36eec63aa09162ccd527cd3d5cd4aa0d93e72
Vadim <promiti@yandex.ru>
2020-02-12 15:40:39 +0300

    ёбанный JS

--- js/script.js
+++ js/script.js
@@ -1,28 +1,57 @@
-let money = prompt('ваш бюджет');
-let name = prompt('название вашего магазина');
+
+let money,  
+    name,
+    time 
+300
+
+function start() {
+  money = prompt('ваш бюджет');
+  while(isNaN(money)|| money == "" || money == null) {
+    money = prompt('ваш бюджет?');
+  }
+  name = prompt('название вашего магазина').toLocaleUpperCase()
+  time = 21;
+}
+
+start();
 
 let mainList = {
   budget: money,
-  shopName: name ,
-  shopGoods: [] ,
+  shopName: name,
+  shopGoods: [],
   employers: {},
   open:false,
+  discount: false
 };
 
-for (let i = 0; i < 5; i++) {
+// цикл ///
+      for (let i = 0; i < 1; i++) {
+        let a = prompt(' какой тип товар будем продовать');
 
-    let a = prompt(' какой тип товар будем продовать');
-    mainList.shopGoods[i] = a;
-    if ((typeof(a)) ==='string' && (typeof(a)) === null && a !='' && a.length <50 ){
-      console.log('всё верно');
-      mainList.shopGoods[i] = a;
-    }
+        if ((typeof(a)) ==='string' && (typeof(a)) === null && a !='' && 
+        a.length < 50 ){
+          console.log( ' всё верно ! ')
+          mainList.shopGoods[i] = a;
+        } 
+    }  
+// chooseGoods();
 
-}
 
-alert(mainList.budget / 30);
 
-console.log(mainList);
+// if (time < 0 ){
+//     console.log (' такого не может быть ! ')
+//   } else if (time > 8 && time < 20){
+//     console.log(' время работать !')
+// }      else if (time < 24 ){
+//         console.log (' уже слишком поздно')
+// }       else {
+//         console.log(' в сутках только 24 часа!!!')
+// }
+
+
+alert( "Ежедневный бюждет " + mainList.budget / 30);
+
+// console.log(mainList);
 
 
 
@@ -73,3 +102,25 @@ console.log(mainList);
 // for(let i = 0; i <8; i++){
 //   console.log(i);
 // }
+
+// let num = 20;
+
+// function showFirstMessage(text){
+//   alert(text);
+//   num = 10;
+// }
+
+
+
+// showFirstMessage("Hello world");
+
+// // console.log(num)
+
+// let calc = (a,b) => a+b 
+
+// console.log (calc(4,5));
+// console.log (calc(4,20));
+
+// let str = "Test"
+// console.log (str.toLocaleLowerCase());
+// console.log (str.toLocaleUpperCase());
