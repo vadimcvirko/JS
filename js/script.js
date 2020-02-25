80dd6691d403a45b369aace4648ac04dfbfdb86a
vadimcvirko <55440551+vadimcvirko@users.noreply.github.com>
2020-02-25 21:30:39 +0300

    Add files via upload
    все кроме сотрудников

--- js/script.js
+++ js/script.js
@@ -1,126 +1,105 @@
+let open = document.getElementById("open-btn"),
+    name_value = document.getElementsByClassName("name-value")[0],
+    budget_value = document.getElementsByClassName("budget-value")[0],
+    goods_value = document.getElementsByClassName('goods-value')[0],
+    items_value = document.getElementsByClassName("items-value")[0],
+    employers_value = document.getElementsByClassName("employers-value")[0],
+    discount_value = document.getElementsByClassName("discount-value")[0],
+    isopen_value = document.getElementsByClassName("isopen-value")[0],
+    goods_item = document.getElementsByClassName("goods-item"),
+    goods_btn = document.getElementsByTagName("button")[1],
+    budget_btn = document.getElementsByTagName("button")[2],
+    employers_btn = document.getElementsByTagName("button")[3],
+    choose_item = document.querySelector('.choose-item'),
+    time_value = document.querySelector('.time-value'),
+    count_budget_value = document.querySelector('.count-budget-value'),
+    hire_employers_item = document.querySelector('.hire-employers-item')
 
-let money,  
-    name,
-    time 
-300
+let money,
+    price;
 
-function start() {
-  money = prompt('ваш бюджет');
-  while(isNaN(money)|| money == "" || money == null) {
-    money = prompt('ваш бюджет?');
+open.addEventListener('click',() => {
+  money = prompt("Ваш бюджет?", "");
+
+  while (isNaN(money) || money == '' || money == null) {
+    money = prompt("Ваш бюджет?", "");
   }
-  name = prompt('название вашего магазина').toLocaleUpperCase()
-  time = 21;
+
+  budget_value.textContent = money;
+
+  name_value.textContent = prompt("Название вашего магазина?","").toUpperCase();
+
+});
+ 
+goods_btn.addEventListener('click',() =>{
+  for(let i = 0; i < goods_item.length; i++ ){
+    let a =goods_item[i].value;
+
+    if ((typeof(a)) === 'string'  && (typeof(a)) != null && a.length < 50 ){
+      console.log(" все верно ");
+      mainList.shopGoods[i] = a;
+      goods_value.textContent = mainList.shopGoods;
+    } else {
+      i = i - 1;
+    }
+  }
+});
+
+choose_item.addEventListener('change',() => {
+    let items = choose_item.value;
+    if (isNaN(items) && items != ''){
+      mainList.shopItems = items.split(', ');
+      mainList.shopItems.sort();
+
+      items_value.textContent = mainList.shopItems;
+
+    }
+});
+
+time_value.addEventListener('change',() => {
+  let time = time_value.value;
+  if (time < 0 ) {
+    console.log ('такого не может быть');
+    mainList.open = false;
+  } else if (time > 8 && time < 20 ){
+    console.log ('время работать!');
+    mainList.open = true;
+  } else if (time < 24 ){
+    console.log('уже слишком поздно');
+    mainList.open = false;
+  } else {
+    console.log ('В сутках только 24 часа');
+    mainList.open = false;
+  };
+  if (mainList.open == true){
+    isopen_value.style.backgroundColor = 'green'
+  } else {
+    isopen_value.style.backgroundColor = 'red'
+  }
+});
+budget_btn.addEventListener('click',() => {
+  count_budget_value.value = money / 30;
+});
+
+employers_btn.addEventListener('click',() => {
+  for ( let i = 0; i < hire_employers_item.length; + i++){
+    let name = hire_employers_item[i].value;
+    mainList.employers[i] = name;
+
+    employers_value.textContent += mainList.employers[i] + ', ';
+  }
+});
+
+const mainList = {
+  budget:money,
+  shopName:name,
+  shopGoods:[],
+  employers:{},
+  open: false,
+  discount: false,
+  shopItems:[],
+  chooseGoods:function chooseGoods(){
+
+  }
 }
 
-start();
-
-let mainList = {
-  budget: money,
-  shopName: name,
-  shopGoods: [],
-  employers: {},
-  open:false,
-  discount: false
-};
-
-// цикл ///
-      for (let i = 0; i < 1; i++) {
-        let a = prompt(' какой тип товар будем продовать');
-
-        if ((typeof(a)) ==='string' && (typeof(a)) === null && a !='' && 
-        a.length < 50 ){
-          console.log( ' всё верно ! ')
-          mainList.shopGoods[i] = a;
-        } 
-    }  
-// chooseGoods();
-
-
-
-// if (time < 0 ){
-//     console.log (' такого не может быть ! ')
-//   } else if (time > 8 && time < 20){
-//     console.log(' время работать !')
-// }      else if (time < 24 ){
-//         console.log (' уже слишком поздно')
-// }       else {
-//         console.log(' в сутках только 24 часа!!!')
-// }
-
-
-alert( "Ежедневный бюждет " + mainList.budget / 30);
-
-// console.log(mainList);
-
-
-
-
-// let num = 50;
-
- 
-// if (num <49 ){
-//   console.log('neverno')
-// } else if(num > 100) {
-//   console.log('neverno')
-// } else {
-//   console.log('нет значения ')
-// }
-
-// (num == 50)? console.log('верно'):console.log('neverno');
-
-
-// switch (num) {
-//   case 49:
-//       console.log('мало');
-//       break;
-//   case 100:
-//       console.log('Много');
-//       break;
-//   case 80:
-//       console.log('всё ещё много ');
-//       break;
-//   case 50:
-//       console.log('в точку');
-//       break;
-//   default:
-//       console.log('не в этот раз');
-//       break;
-// }
-
-// while (num <55) {
-//   alert(num)
-//   num++;
-// }
-
-// do {
-//   console.log(num);
-//   num++;
-// }
-// while(num <55);
-
-// for(let i = 0; i <8; i++){
-//   console.log(i);
-// }
-
-// let num = 20;
-
-// function showFirstMessage(text){
-//   alert(text);
-//   num = 10;
-// }
-
-
-
-// showFirstMessage("Hello world");
-
-// // console.log(num)
-
-// let calc = (a,b) => a+b 
-
-// console.log (calc(4,5));
-// console.log (calc(4,20));
-
-// let str = "Test"
-// console.log (str.toLocaleLowerCase());
-// console.log (str.toLocaleUpperCase());
